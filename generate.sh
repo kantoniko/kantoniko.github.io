998f698f0d14e238bb6b7dca32bc8d1b9c3b8ea9
Gabor Szabo <gabor@szabgab.com>
2023-09-05 08:50:13 +0300

    propagate errors

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
+set -ex
 export PYTHONPATH=ladino-diksionaryo-code
 python ladino-diksionaryo-code/ladino/generate.py --dictionary ladino-diksionaryo-data/ --html _site --all --log --whatsapp ladino-estamos-whatsapeando --sounds ladino-diksionaryo-sounds --unafraza ladino-una-fraza-al-diya --pages ladino-pages --books ladino-salu-lulu --ladinadores ladino-los-ladinadores --enkontros enkontros-de-alhad
 echo kantoniko.com > _site/CNAME
